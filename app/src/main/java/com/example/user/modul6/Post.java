2524282be9feb7d8cb7b0497a2e3c7311292a010
novirp13 <file:///E:/StudyCase/>
2018-04-02 00:01:51 +0700

    push pertama kali

---
+++ app/src/main/java/com/example/user/modul6/Post.java
@@ -0,0 +1,44 @@
+package com.example.user.modul6;
+
+/**
+ * Created by Chandratya on 01/04/2018.
+ */
+
+public class Post {
+    private String username;
+    private String photo;
+    private String photoTitle;
+    private String photoDesc;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getPhotoTitle() {
+        return photoTitle;
+    }
+
+    public void setPhotoTitle(String photoTitle) {
+        this.photoTitle = photoTitle;
+    }
+
+    public String getPhotoDesc() {
+        return photoDesc;
+    }
+
+    public void setPhotoDesc(String photoDesc) {
+        this.photoDesc = photoDesc;
+    }
+}
